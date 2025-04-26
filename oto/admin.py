81a0311d84086a11d7692a0f96451c683a960fbd
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2025-04-27 02:19:07 +0300

    oto_update

--- oto/admin.py
+++ oto/admin.py
@@ -4,20 +4,20 @@ from django.contrib import admin
 from oto.models import Place,Restaurant,Waiter
 # Register your models here.
 
-# @admin.register(Place)
-class Place_Admin(admin.ModelAdmin):
-        all_fields = [f.name for f in Place._meta.fields]+["id"]                  #SHOW ALL FILDES
-        list_display=all_fields
-        readonly_fields=('id',)
+# # @admin.register(Place)
+# class Place_Admin(admin.ModelAdmin):
+#         all_fields = [f.name for f in Place._meta.fields]+["id"]                  #SHOW ALL FILDES
+#         list_display=all_fields
+#         readonly_fields=('id',)
 
-class Restaurant_Admin(admin.ModelAdmin):
-        all_fields = [f.name for f in Restaurant._meta.fields]                #SHOW ALL FILDES
-        list_display=all_fields
+# class Restaurant_Admin(admin.ModelAdmin):
+#         all_fields = ["pk"]+[f.name for f in Restaurant._meta.fields]                #SHOW ALL FILDES
+#         list_display=all_fields
 
-class Waiter_Admin(admin.ModelAdmin):
-        all_fields = [f.name for f in Waiter._meta.fields]+["id"]                  #SHOW ALL FILDES
-        list_display=all_fields     
+# class Waiter_Admin(admin.ModelAdmin):
+#         all_fields = [f.name for f in Waiter._meta.fields]+["id"]                  #SHOW ALL FILDES
+#         list_display=all_fields     
                    
-admin.site.register(Place,Place_Admin)
-admin.site.register(Restaurant,Restaurant_Admin)
-admin.site.register(Waiter,Waiter_Admin)
+# admin.site.register(Place,Place_Admin)
+# admin.site.register(Restaurant,Restaurant_Admin)
+# admin.site.register(Waiter,Waiter_Admin)
